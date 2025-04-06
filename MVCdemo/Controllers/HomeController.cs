bdc3fcb78ac63fd3293b8fdbc48c37229a30faae
Leeloo <leeloo.schaff@gmail.com>
2025-04-06 23:26:33 +1000

    Update Layout!!

--- MVCdemo/Controllers/HomeController.cs
+++ MVCdemo/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MVCdemo.Data;
 using MVCdemo.Models;
 
 namespace MVCdemo.Controllers
@@ -29,10 +30,18 @@ namespace MVCdemo.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }*/
 
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-            return View();
+            //ViewBag.Title = "Home Page";
+            var students = _context.Students.ToList();
+            return View(students);
         }
 
         public ActionResult About()
